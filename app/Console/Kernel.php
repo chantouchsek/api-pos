51926ebe69712822e0b112f581235d2540bef45e
Touch <s.chantouch@mango-byte.com>
2019-03-13 16:07:38 +0700

    test queue

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('queue:checkup')->everyFiveMinutes();
+        $schedule->command('queue:checkup')->everyMinute();
     }
 
     /**
